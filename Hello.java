e99d1bcda613e43762ec81e7b0087b4924f22a7e
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-02-28 05:27:09 -0800

    Airthmetic operators

--- Hello.java
+++ Hello.java
@@ -2,9 +2,21 @@ class Hello
 {
     public static void main(String a[])
     {
-        int num1 = 3;
-        int num2 = 5;
-        int result = num1 * num2;
+        int num1 = 7;
+        // int num2 = 5;
+        // int result = num1 % num2; // ("/" will give you Quotient and "%" will give remainder)
+
+        // num1 = num1 + 2;  
+        
+        // num1 += 2;
+        // num1 += 1;
+        // num1--;        // decrement
+        // num1++;        // post-increment
+        // ++num1;        // pre-increment
+
+        // int result = ++num1 ; // Its answer is 8 becuse it first increment and then fetch the value
+        int result = num1++ ; // Its answer is 7 because first its fetch the value and then increment
+
         System.out.println(result);
     }
 }
